34d47c4aefddbd2140c3015a766d0768b6e8b039
Nthulane Makgato <nthulanemakgato@Nthulanes-MacBook-Pro.local>
2023-01-10 22:09:43 +0200

    create private route and custom hook

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,60 @@
-import React from "react";
+import { getAuth } from "firebase/auth";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Profile() {
-  return <div>Profile</div>;
+  const navigate = useNavigate();
+  const auth = getAuth();
+  const [formData, SetFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
+
+  const { name, email } = formData;
+
+  function onLogout() {
+    auth.signOut();
+    navigate("/");
+  }
+
+  return (
+    <>
+      <section className="max-w-6x1 mx-auto flex justify-items-center items-center flex-col">
+        <h1 className="text-3x1 text-center mt-6 font-bold">My Profile</h1>
+        <div className="w-full md:w-[50%] mt-6 px-3">
+          <form action="">
+            <input
+              type="text"
+              id="name"
+              value={name}
+              className="mb-6 w-full px-4 py-2 text-xl text-grey-700 bg-white border border-gray-300 rounded transition ease-in-out"
+            />
+            <input
+              type="text"
+              id="email"
+              value={email}
+              className="mb-6 w-full px-4 py-2 text-xl text-grey-700 bg-white border border-gray-300 rounded transition ease-in-out"
+            />
+
+            <div className="mb-6 flex justify-between whitespace-nowrap text-sm sm:text-lg">
+              <p className="flex items-center">
+                Do you want to change your name?{" "}
+                <span className="text-red-500 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer">
+                  Edit
+                </span>
+              </p>
+              <p
+                onClick={onLogout}
+                className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer"
+              >
+                Sign out
+              </p>
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Profile;
